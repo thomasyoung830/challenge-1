fc571eeea46de7cc0bb1228ccd3ffe2d279e7463
eatpie75 <eatpie75@gmail.com>
2015-07-16 13:33:29 -0700

    [feat] implement user_info endpoint

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,36 @@ router.get('/logout');
 router.get('/auth/facebook');
 router.get('/auth/facebook/callback');
 
-router.get('/user_info');
+/**
+ * Endpoint to get information about logged in user
+ */
+router.get('/user_info', function(req, res) {
+  /**
+   * Check if user is logged in and return an error if not
+   */
+  // if (!req.isAuthenticated) {
+  //   res.json({'error':'Endpoint requires login.'});
+  //   return;
+  // }
+
+  /**
+  * Object returned from endpoint
+  */
+  // var data = {
+  //   'first_name': req.user.first_name,
+  //   'last_name': req.user.last_name,
+  //   'profile_image' : req.user.profile_image,
+  // };
+
+  // Mock data
+  var data = {
+    // 'id': 1,
+    'first_name': 'Randy',
+    'last_name': 'Savage',
+    'profile_image' : 'http://img.bleacherreport.net/img/images/photos/001/866/715/randy_savage_crop_north.png?w=377&h=251&q=75',
+  };
+  res.json(data);
+});
 
 router.get('/challenge/user');
 router.get('/challenge/public');
