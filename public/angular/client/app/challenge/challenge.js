e3188957f0c25ac9920855fd675029324b551190
mjsgreen01 <mjsgreen01@gmail.com>
2015-07-17 13:40:00 -0700

    [feat] challenge factory and controller functionality

--- public/angular/client/app/challenge/challenge.js
+++ public/angular/client/app/challenge/challenge.js
@@ -1,5 +1,18 @@
 angular.module('challengeApp.challenge', [])
 
-.controller('ChallengeController', function ($scope, Challenge) {
-  // Your code here
+.controller('ChallengeController', function ($scope, ChallengeFactory) {
+  
+  $scope.challengeData = {};
+  
+  $scope.getChallengeInfo = function(challengeId){
+    ChallengeFactory.getChallengeInfo(challengeId).then(function(res){
+        $scope.challengeData.creator = res.creator;
+        $scope.challengeData.participants = res.participants[0].id;
+        $scope.challengeData.title = res.title;
+        $scope.challengeData.description = res.message;
+    });
+  };
+
+
+
 });
--- public/angular/client/app/services/services.js
+++ public/angular/client/app/services/services.js
@@ -3,3 +3,20 @@ angular.module('challengeApp.services', [])
 .factory('Auth', function ($http) {
   // Your code here
 })
+
+.factory('ChallengeFactory', function($http){
+	var getChallengeInfo = function(challengeId){
+		return $http({
+		  method: 'GET',
+		  url: '/challenge/' +  challengeId,
+		}).then(function(resp){
+		  return resp;
+		});
+	};
+
+
+
+	return {
+		getChallengeInfo: getChallengeInfo
+	};
+});
