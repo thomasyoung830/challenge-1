c86e67b891825db97e8299bd9eabd5cc986e325c
mjsgreen01 <mjsgreen01@gmail.com>
2015-07-20 18:02:34 -0700

    [feat] basic create-challenge page set up

--- public/angular/client/challengerApp/create/create.js
+++ public/angular/client/challengerApp/create/create.js
@@ -1,5 +1,24 @@
 angular.module('challengeApp.createChallenge', [])
 
-.controller('createChallengeController', function ($scope, createChallenge) {
-  // Your code here
+.controller('CreateChallengeController', function ($scope, CreateChallengeFactory) {
+  
+  $scope.allUsers = [];
+  $scope.challengeInfo = {};
+
+  // get array of all users in the database
+  CreateChallengeFactory.getAllUsers().then(function(res){
+    $scope.allUsers = res;
+    // for testing purposes, set the challengee to the first user in allUsers
+    $scope.challengee = $scope.allUsers[0];
+  });
+
+  // get the info of the person creating the challenge
+  CreateChallengeFactory.getCreatorInfo().then(function(res){
+    $scope.creatorUser = res;
+  });
+
+  // method that takes the challengeInfo object as argument and calls the factory POST call
+
+
+
 });
